ebe81cd530907f2ae91143af2dddb5d3bb1da2a6
Karina Cygan <karinacygan06@gmail.com>
2026-03-17 18:29:41 +0100

    Add recipe search with database integration

--- webapp/main/models.py
+++ webapp/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    category = models.CharField(max_length=100, blank=True)
+    prep_time = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
